73ab732544372df2de54dc2b0f713d97b08e81ce
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2022-09-08 21:20:51 -0700

    style fix
    Signed-off-by: Oleksii Kuchaiev <okuchaiev@nvidia.com>

--- examples/nlp/language_modeling/megatron_gpt_prompt_learning_eval.py
+++ examples/nlp/language_modeling/megatron_gpt_prompt_learning_eval.py
@@ -12,19 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import torch
 from omegaconf import OmegaConf
 from omegaconf.omegaconf import open_dict
 from pytorch_lightning.trainer.trainer import Trainer
-from nemo.collections.nlp.parts.nlp_overrides import NLPSaveRestoreConnector
 
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_prompt_learning_model import (
     MegatronGPTPromptLearningModel,
 )
 from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam, SamplingParam
-from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy
+from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy, NLPSaveRestoreConnector
 from nemo.core.config import hydra_runner
-import os
 
 
 """
@@ -90,7 +90,9 @@ def main(cfg) -> None:
         save_resotre_connector.model_extracted_dir = cfg.gpt_model_file
 
     prompt_learning_cfg = MegatronGPTPromptLearningModel.restore_from(
-        cfg.virtual_prompt_model_file, trainer=trainer, return_config=True,
+        cfg.virtual_prompt_model_file,
+        trainer=trainer,
+        return_config=True,
         save_restore_connector=save_resotre_connector,
     )
     with open_dict(prompt_learning_cfg):
@@ -100,7 +102,9 @@ def main(cfg) -> None:
 
     # Now load prompt learning model with frozen gpt model base
     model = MegatronGPTPromptLearningModel.restore_from(
-        restore_path=cfg.virtual_prompt_model_file, trainer=trainer, override_config_path=prompt_learning_cfg,
+        restore_path=cfg.virtual_prompt_model_file,
+        trainer=trainer,
+        override_config_path=prompt_learning_cfg,
         save_restore_connector=save_resotre_connector,
     )
 
